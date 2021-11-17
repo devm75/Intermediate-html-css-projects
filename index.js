0feb256682c06d463874f7dd33a76e21148fe919
Mohan Dev <bhardwaj1206409@gmail.com>
2021-11-17 13:00:25 +0530

    version-1

--- index.js
+++ index.js
@@ -4,72 +4,52 @@ const about = document.getElementById("about-section");
 const experience = document.getElementById("experience-section");
 const sections = document.querySelectorAll(".section");
 const landingViewSection = document.querySelector(".landing-view-section");
+const elementsToAnimate = document.querySelectorAll("[data-animate_upwards]");
+const navItems = navbar.querySelectorAll('#nav-item');
+//---scroll based navbar modifications--//
+navItems.forEach((element) => element.addEventListener('click', (e) => {
+  e.preventDefault();
+  const idOfElementToScrollTo = e.target.getAttribute('href');
 
-//--------Navbar Effects-----------//
+   document.querySelector(idOfElementToScrollTo).scrollIntoView({behavior:'smooth'})
+}
+))
+// navbar.querySelector()
+
 
 document.addEventListener("scroll", (e) => {
-  let distanceFromTopOfIntroHeader = intro.getBoundingClientRect();
-
-  if (distanceFromTopOfIntroHeader.y < 300) {
-    document.getElementById("navbar").style.backgroundColor = "white";
-    document.getElementById("navbar").style.zIndex = 100;
-    document.getElementById("navbar").style.paddingBottom = "1rem";
-    document.getElementById("navbar").style.borderBottom =
-      "2px solid rgb(243, 226, 226)";
+  if (scrollY > 80) {
+    navbar.style.backgroundColor = "white";
+    const navItemArray = navbar.children;
+    for (let element of navItemArray) {
+      element.style.transform = "translateY(-20px)";
+    }
   } else {
-    document.getElementById("navbar").style.backgroundColor = "";
-    document.getElementById("navbar").style.zIndex = 1;
-    document.getElementById("navbar").style.paddingBottom = "0";
-    document.getElementById("navbar").style.borderBottom = "none";
-  }
-
-  let distanceFromTopOfAboutSection = about.getBoundingClientRect();
-
-  if (distanceFromTopOfAboutSection.y < 1000) {
-    document.getElementById("about-image-container").style.justifyContent =
-      "center";
+    navbar.style.backgroundColor = "";
+    const navItemArray = navbar.children;
+    for (let element of navItemArray) {
+      element.style.transform = "translateY(0)";
+    }
   }
 });
 
-//-----------scroll effect on all elements, move upwards-----//
+//-----------scroll effect on all elements, move upwards-----Intersection Observer API--//
 
 const options = {
   root: null,
-  threshold: 0.15,
+  threshold: 0.1,
   rootMargin: "10px",
 };
 
 let observer1 = new IntersectionObserver(function (entries, observer) {
   entries.forEach((entry) => {
-    if (
-      entry.isIntersecting &&
-      !entry.target.classList.contains("landing-view-section")
-    ) {
-      console.log(entry);
-      entry.target.style.transition = "transform 2s";
+    if (entry.isIntersecting) {
       entry.target.style.transform = "translateY(0)";
-      // console.log('hi');
-
-      observer.unobserve(entry.target);
-    }
-
-    if (entry.target.querySelector(".intro") && entry.isIntersecting) {
-      console.log("hi");
-      document.querySelector("#intro").style.transition = "transform 1.5s";
-      document.querySelector("#intro").style.transform = "translateY(-50px)";
-
-      console.log(entry.target);
-    } else {
-      console.log("bye");
-    }
-
-    if (entry.target.querySelector(".image-border") && entry.isIntersecting) {
-      document.querySelector(".image-border").style.transform =
-        "translate(0,0)";
+      observer1.unobserve(entry.target);
     }
   });
 }, options);
 
-sections.forEach((section) => {
-  observer1.observe(section);
+elementsToAnimate.forEach((element) => {
+  observer1.observe(element);
 });
